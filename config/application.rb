155249502ce4ef81e6e403e3ff4f51255806a381
Jacob Rockow <jrrockow@gmail.com>
2024-05-25 15:04:38 -0400

    css compressor setting

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module JacobsWebsite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.css_compressor = nil
+
+
   end
 end
